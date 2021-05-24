d23aa18a6166091d004faa4162d52790fdd31af8
Daniel Reis <hey@danielheart.dev>
2021-05-24 20:14:55 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,2 +1,2 @@
 <?php
-  echo "vi minha ex hoje e foi muito estranho terminamos a 1 ano e achava nao sentir nada e agora to meio estranho, sei que nao gosto dela mas mo sentimento esquisito, oq fazer"
+  echo "vi minha ex hoje e foi muito estranho terminamos a 1 ano e achava nao sentir nada e agora to meio estranho, sei que nao gosto dela mas mo sentimento esquisito, oq fazer";
